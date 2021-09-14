9f682b62727b662fe1e5557a41889aa038adc5ff
IT21009204 <87411477+IT21009204@users.noreply.github.com>
2021-09-14 15:59:57 +0000

    i am finishe my tutoril 7

--- Item.cpp
+++ Item.cpp
@@ -3,11 +3,26 @@
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
-// 5. Implement Overloaded Constructor Implementation
-// 6. Implement Destructor (display "Destructor Called")
+ Item::Item(){
+   itemCode = 0;
+   unitPrice= 0;
+   discount = 0;
+
+ }
+ // 5. Implement Overloaded Constructor Implementation
+  
+  Item::Item(  int I_code,float I_initprice ){
+    itemCode = I_code;
+    unitPrice = I_initprice;
+   // discount = I_DIS;
+    
+  }
+
 
 void Item::setDiscount(float pdiscount) {
+
   discount = pdiscount;
+
 }
 
 float Item::getDiscount() {
@@ -20,5 +35,13 @@ float Item::discountedPrice() {
 
 void Item::display() {
   cout << "Item : " << itemCode << endl;
-  cout << "Discounted Price " << discountedPrice() << endl;
+  cout << "Discounted Price :" << discountedPrice() << endl;
+}
+
+// 6. Implement Destructor (display "Destructor Called")
+
+Item::~Item(){
+ 
+  cout<< "desturcotor call " << " Itam code = " << itemCode<< " discount price : " << discountedPrice()<<endl;
+
 }
--- Item.h
+++ Item.h
@@ -7,11 +7,16 @@ class Item {
       float discount; // out of 100 e.g. discount = 15
    public:
       // 1. Write the Default Constructor
-      // 2. Write the Overloaded Constructor
-      // 3. Write the Destructor
-
+      Item();
+    // 2. Write the Overloaded Constructor
+      Item(int I_code, float I_initprice);
+  
       void setDiscount(float punitPrice);
       float getDiscount();
       float discountedPrice();
       void display();
+
+      // 3. Write the Destructor
+      ~ Item();
+
 };
